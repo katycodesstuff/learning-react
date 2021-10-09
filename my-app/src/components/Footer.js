98813e7ff26ce2ad1ddbfe5ff7b0e57616032e1a
katycodesstuff <33542984+katycodesstuff@users.noreply.github.com>
2021-10-09 16:32:13 +0100

    Fix issue with imports which meant footer could not render

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import { BottomNavigation, Typography } from '@mui/material'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import { Facebook } from '@mui/icons-material/Facebook'
-import { Twitter } from '@mui/icons-material/Twitter'
-import { Instagram } from '@mui/icons-material/Instagram'
-import { YouTube } from '@mui/icons-material/YouTube'
+import { Facebook, Twitter, Instagram, YouTube } from '@mui/icons-material'
 
 function Footer() {
     return (
         <BottomNavigation>
-            {/* The below won't work for some reason*/}
-            {/* <BottomNavigationAction label="Facebook" icon={<Facebook />} />
-            <BottomNavigationAction label="Twitter" icon={<Twitter />} />
-            <BottomNavigationAction label="Instagram" icon={<Instagram/>} />
-            <BottomNavigationAction label="YouTube" icon={<YouTube />} /> */}
+            <BottomNavigationAction label="Facebook" icon={<Facebook style={{fill: "#3b5998"}}/>} />
+            <BottomNavigationAction label="Twitter" icon={<Twitter style={{fill: "#1da1f2"}}/>} />
+            <BottomNavigationAction label="Instagram" icon={<Instagram style={{fill: "#c13584"}}/>} />
+            <BottomNavigationAction label="YouTube" icon={<YouTube style={{fill: "#c4302b"}}/>} />
             <Typography color="primary">Thanks for looking at my first attempt using React!</Typography>
         </BottomNavigation>
     );
