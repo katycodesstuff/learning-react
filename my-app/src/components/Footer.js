1fd97fd0bd63bfb34b29618088f65fc22e2c3e52
katycodesstuff <33542984+katycodesstuff@users.noreply.github.com>
2021-10-09 16:48:09 +0100

    Set bg colour for footer and set icon colours

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import { BottomNavigation, Typography } from '@mui/material'
+import { BottomNavigation } from '@mui/material'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import { Facebook, Twitter, Instagram, YouTube } from '@mui/icons-material'
 
 function Footer() {
     return (
-        <BottomNavigation>
-            <BottomNavigationAction label="Facebook" icon={<Facebook style={{fill: "#3b5998"}}/>} />
-            <BottomNavigationAction label="Twitter" icon={<Twitter style={{fill: "#1da1f2"}}/>} />
-            <BottomNavigationAction label="Instagram" icon={<Instagram style={{fill: "#c13584"}}/>} />
-            <BottomNavigationAction label="YouTube" icon={<YouTube style={{fill: "#c4302b"}}/>} />
-            <Typography color="primary">Thanks for looking at my first attempt using React!</Typography>
+        <BottomNavigation style={{ backgroundColor: '#385D81'}}>
+            <BottomNavigationAction label="Facebook" icon={<Facebook style={{fill: "secondary"}}/>} />
+            <BottomNavigationAction label="Twitter" icon={<Twitter style={{fill: "secondary"}}/>} />
+            <BottomNavigationAction label="Instagram" icon={<Instagram style={{fill: "secondary"}}/>} />
+            <BottomNavigationAction label="YouTube" icon={<YouTube style={{fill: "secondary"}}/>} />
         </BottomNavigation>
     );
 }
