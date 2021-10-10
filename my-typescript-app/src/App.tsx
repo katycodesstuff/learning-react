b997daf84d1b4308d49e74d27e2bd974d4e67cf3
katycodesstuff <33542984+katycodesstuff@users.noreply.github.com>
2021-10-10 11:43:42 +0100

    Add TS react project. So far very basic nav bar is displayed

---
+++ my-typescript-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+    </div>
+  );
+}
+
+export default App;
